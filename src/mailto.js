0266e1c3fdbda8dbb022cfe2208020adf4528c03
Paul Sammut <paul@pop-os.localdomain>
2020-03-20 15:02:50 +1100

    wild-queen

--- src/mailto.js
+++ src/mailto.js
@@ -3,10 +3,10 @@ const subject = encodeURIComponent("Shop stock completed");
 const body = encodeURIComponent(`
 
 Open app:
-[https://squalid-stage.surge.sh] 
+https://wild-queen.surge.sh 
 
 Open spreadsheet:
-[https://docs.google.com/spreadsheets/d/19e2yItX7uHtERCNbq42yC4BjcUQ3rSQUmyDPwv8Uhlg/edit?usp=sharing]
+https://docs.google.com/spreadsheets/d/19e2yItX7uHtERCNbq42yC4BjcUQ3rSQUmyDPwv8Uhlg/edit?usp=sharing
 
 `);
 
