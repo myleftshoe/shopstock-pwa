9b98d56ad668b44d01cf297ef60182e851aacc67
Paul Sammut <paul@pop-os.localdomain>
2020-03-24 11:43:32 +1100

    uid.js put this.excluded in constructor (was usin experimental class properties)

--- src/uid.js
+++ src/uid.js
@@ -1,34 +1,31 @@
-const safe = '23456789abdegjkmnpqrvwxyz';
+const safe = '23456789abdegjkmnpqrvwxyz'
 const alpha = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
 
-const uid = { 
-    generate(charset = safe, length = 8) {
-        let result = '';
-        for (let i = 0; i < length; i++) {
-            result += charset.charAt(Math.floor(Math.random() * charset.length));
-        }
-        return result;
-    }
-}
-
-export { safe, alpha };
-export default uid;
-
-
-class uid {
-    constructor(charset = safe, length = 8) {
-        this.charset = charset;
-        this.length = length;
+class UID {
+    constructor({ charset = safe, length = 8 } = {}) {
+        this.charset = charset
+        this.length = length
+        this.excluded = new Set();
     }
     exclude(uids = []) {
-        this.excluded = new Set(uids);
-        return this;
+        this.excluded = new Set(uids)
+        return this
+    }
+    generate() {
+        let result = ''
+        for (let i = 0; i < this.length; i++) {
+            result += this.charset.charAt(Math.floor(Math.random() * this.charset.length))
+        }
+        return result
     }
     get value() {
-        let result = '';
-        for (let i = 0; i < length; i++) {
-            result += this.charset.charAt(Math.floor(Math.random() * this.charset.length));
-        }
-        return result;
+        let result = ''
+        do {
+            result = this.generate()
+        } while (this.excluded.has(result))
+        return result
     }
 }
+
+export { safe, alpha }
+export default UID
