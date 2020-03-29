762575a2a445700f618521d01ec0c025feb28f3c
Paul Sammut <paul@pop-os.localdomain>
2020-03-30 10:15:36 +1100

    formating

--- src/search.svelte
+++ src/search.svelte
@@ -1,39 +1,44 @@
 <script>
-    import { onMount, onDestroy } from "svelte"
+    import { onMount, onDestroy } from 'svelte'
     import debounce from 'lodash-es/debounce'
-    
-    export let value=""
-    
-    let input = {};
 
-    const debounced = debounce(e => value = e.target.value, 300)
+    export let value = ''
+
+    let input = {}
+
+    const debounced = debounce(e => (value = e.target.value), 300)
     onMount(() => input.addEventListener('input', debounced))
     onDestroy(() => input.removeEventListener('input', debounced))
 
-    let placeholder = "";
+    let placeholder = ''
 
-    const showPlaceholder = () => placeholder = "SEARCH"
-    const hidePlaceholder = () => placeholder = ""
-
+    const showPlaceholder = () => (placeholder = 'SEARCH')
+    const hidePlaceholder = () => (placeholder = '')
 </script>
 
-<i class="fas fa-search" on:click={() => input.focus()}/>
-<input bind:this={input} type="search" placeholder={placeholder} on:focus={showPlaceholder} on:blur={hidePlaceholder}>
+<i class="fas fa-search" on:click={() => input.focus()} />
+<input
+    bind:this={input}
+    type="search"
+    {placeholder}
+    on:focus={showPlaceholder}
+    on:blur={hidePlaceholder}
+/>
 
 <style>
     input {
         background: none;
-        border:none;
+        border: none;
         outline: none;
         padding: 4px;
         margin: 4px;
         color: #ddd;
-        font-weight:600;
+        font-weight: 600;
     }
     input:focus {
         border-bottom: 1px solid #ddd;
     }
     i {
-        padding:8px;
+        padding: 8px;
     }
 </style>
