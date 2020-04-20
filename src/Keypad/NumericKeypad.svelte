19ed1a928fd345861c0a54f4ccc87c7d5a02dcac
Paul Sammut <paul@pop-os.localdomain>
2020-04-20 10:18:11 +1000

    Use unicode fractions in NumericKeypad.svelte

--- src/Keypad/NumericKeypad.svelte
+++ src/Keypad/NumericKeypad.svelte
@@ -9,25 +9,23 @@
     <Key type="number" value="1" />
     <Key type="number" value="2" />
     <Key type="number" value="3" />
-    <Key type="fraction" value="1/4" />
+    <Key type="fraction" value="&frac14" />
 </KeyRow>
 <KeyRow>
     <Key type="number" value="4" />
     <Key type="number" value="5" />
     <Key type="number" value="6" />
-    <Key type="fraction" value="1/2" />
+    <Key type="fraction" value="&frac12" />
 </KeyRow>
 <KeyRow>
     <Key type="number" value="7" />
     <Key type="number" value="8" />
     <Key type="number" value="9" />
-    <Key type="fraction" value="3/4" />
+    <Key type="fraction" value="&frac34" />
 </KeyRow>
 <KeyRow>
     <Key type="increment" value="+" />
     <Key type="number" value="0" />
-    <Key type="decrement" value="-" />
-    <Key type="clear" value="X">
-        <Icon><BackspaceIcon/></Icon>
-    </Key>
+    <Key type="decrement" value="-">&minus</Key>
+    <Key type="clear" value="X"><Icon><BackspaceIcon/></Icon></Key>
 </KeyRow>
